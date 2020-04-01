01786e650470314065e445d502e4d4d3efb8a370
Andres Valero <avaleror@mordor.home>
2020-04-01 11:55:54 +0200

    Scripts to set and unset http proxy

---
+++ Bash/set_http_proxy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PROXY='10.31.3.147'
+
+#Set proxy vars
+export HTTP_PROXY=$PROXY
+export HTTPS_PROXY=$PROXY
+export http_proxy=$PROXY
+export https_proxy=$PROXY
---
+++ Bash/unset_http_proxy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Unset proxy vars
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset http_proxy
+unset https_proxy
